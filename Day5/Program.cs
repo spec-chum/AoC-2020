2363ff4ea4af85032de9d28003a073ceaa86b583
Spec-Chum <specchumster@gmail.com>
2020-12-05 19:13:20 +0000

    Optimised day 5 further

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace Day5
 {
@@ -9,10 +10,9 @@ namespace Day5
 		{
 			var input = File.ReadAllLines("Day5.txt");
 
-			int[] seats = new int[input.Length];
-
-			// Part 1
+			int lowest = int.MaxValue;
 			int highest = 0;
+			int sum = 0;
 			for (int i = 0; i < input.Length; i++)
 			{
 				string seat = input[i];
@@ -25,23 +25,25 @@ namespace Day5
 					}
 				}
 
-				seats[i] = id;
+				sum += id;
 
 				if (id > highest)
 				{
 					highest = id;
 				}
+				else if (id < lowest)
+				{
+					lowest = id;
+				}
 			}
 
+			// Part 1
 			Console.WriteLine($"Highest seat ID: {highest}");
 
 			// Part 2
-			Array.Sort(seats);
-
-			int k = 1;
-			for (; seats[k] - 1 == seats[k - 1]; k++) ;
-
-			Console.WriteLine($"My seat ID: {seats[k] - 1}");
+			int n = input.Length;
+			int mySeat = ((n + 1) * (n + (2 * lowest)) / 2) - sum;
+			Console.WriteLine($"My seat: {mySeat}");
 		}
 	}
 }
