f3cd4619ead18bcbc0e9c4511ec06a1be5c9ec25
Spec-Chum <specchumster@gmail.com>
2020-12-07 00:01:50 +0000

    Added Day 6 solution

---
+++ Day6/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+
+namespace Day6
+{
+	class Program
+	{
+		static void Main()
+		{
+			var input = File.ReadAllText("Day6.txt").Split("\n\n");
+
+			int totalAnswers = 0;
+			int totalQuestions = 0;
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				var answerFull = input[i].Replace("\n", "");
+				var answerDistinct = RemoveDuplicates(answerFull);
+
+				// Part 1
+				totalAnswers += answerDistinct.Length;
+
+				// Part 2
+				var numPeople = Count(input[i], '\n') + 1;
+				
+				// Handle last line not having \n\n
+				if (i == input.Length - 1)
+				{
+					numPeople--;
+				}
+
+				for (int k = 0; k < answerDistinct.Length; k++)
+				{
+					if (Count(answerFull, answerDistinct[k]) == numPeople)
+					{
+						totalQuestions++;
+					}
+				}
+			}
+
+			Console.WriteLine($"Total number of answers: {totalAnswers}");
+			Console.WriteLine($"Number of questions groups all answered yes to: {totalQuestions}");
+		}
+		static ReadOnlySpan<char> RemoveDuplicates(ReadOnlySpan<char> str)
+		{
+			char[] result = new char[str.Length];
+
+			int j = 0;
+			for (int i = 0; i < str.Length; i++)
+			{
+				if (!Contains(result, str[i]))
+				{
+					result[j++] = str[i];
+				}
+			}
+
+			return result.AsSpan().Slice(0, j);
+		}
+
+		private static bool Contains(ReadOnlySpan<char> str, char letter)
+		{
+			for (int i = 0; i < str.Length; i++)
+			{
+				if (str[i] == letter)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		static int Count(string str, char letter)
+		{
+			int count = 0;
+
+			for (int i = 0; i < str.Length; i++)
+			{
+				if (str[i] == letter)
+				{
+					count++;
+				}
+			}
+
+			return count;
+		}
+	}
+}
