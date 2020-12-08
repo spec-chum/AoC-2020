850e14682699f264a268484d3e0532ace4f84440
Spec-Chum <specchumster@gmail.com>
2020-12-08 16:40:55 +0000

    Day 8  - Part 2 done

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -1,23 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Day8
 {
 	class Program
 	{
+		static readonly string[] input = File.ReadAllLines("Day8.txt");
+		static readonly bool[] visited = new bool[input.Length + 1];
+
 		static int A;
 		static int PC;
 
 		static void Main(string[] args)
 		{
-			var input = File.ReadAllLines("Day8.txt");
-			var visited = new bool[input.Length];
+			visited[input.Length] = true;
 
-			while(!visited[PC])
+			Execute();
+			Console.WriteLine($"Part 1: A = {A}");
+
+			Part2();
+			Console.WriteLine($"Part 2: A = {A}");
+		}
+
+		private static void Execute()
+		{
+			Array.Clear(visited, 0, visited.Length - 1);
+			A = 0;
+			PC = 0;
+
+			while (!visited[PC])
 			{
 				visited[PC] = true;
 
-				var opcode = input[PC];
+				string opcode = input[PC];
 				int offset = int.Parse(opcode[4..]);
 
 				if (opcode.StartsWith("acc"))
@@ -34,8 +50,61 @@ namespace Day8
 					PC++;
 				}
 			}
+		}
 
-			Console.WriteLine(A);
+		private static void Part2()
+		{
+			List<int> nops = new();
+			PopulateList(nops, "nop");
+			for (int i = 0; i < nops.Count; i++)
+			{
+				int nop = nops[i];
+				string temp = input[nop];
+				input[nop] = input[nop].Replace("nop", "jmp");
+
+				Execute();
+				if (PC == input.Length)
+				{
+					Console.WriteLine($"Execution successful by changing NOP at index {i} to JMP");
+					return;
+				}
+				else
+				{
+					input[nop] = temp;
+				}
+			}
+
+			List<int> jmps = new();
+			PopulateList(jmps, "jmp");
+			for (int i = 0; i < jmps.Count; i++)
+			{
+				int jmp = jmps[i];
+				string temp = input[jmp];
+				input[jmp] = input[jmp].Replace("jmp", "nop");
+
+				Execute();
+				if (PC == input.Length)
+				{
+					Console.WriteLine($"Execution successful by changing JMP at index {i} to NOP");
+					return;
+				}
+				else
+				{
+					input[jmp] = temp;
+				}
+			}
+		}
+
+		static void PopulateList(List<int> list, string mnenomic)
+		{
+			for (int i = 0; i < input.Length; i++)
+			{
+				string opcode = input[i][..3];
+				if (opcode == mnenomic)
+				{
+					list.Add(i);
+				}
+			}
 		}
 	}
 }
