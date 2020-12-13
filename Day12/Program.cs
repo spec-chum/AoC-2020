06f28efc07718d1d42702383d8afa456ca9abcb4
Spec-Chum <specchumster@gmail.com>
2020-12-13 03:40:25 +0000

    Merged parts 1 and 2

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -5,83 +5,32 @@ namespace Day12
 {
 	class Program
 	{
-		enum Direction
-		{
-			North,
-			East,
-			South,
-			West
-		}
-
 		static void Main(string[] args)
 		{
 			var input = File.ReadAllLines("Day12.txt");
 
-			Direction dir = Direction.East;
-			(int x, int y) location = (0, 0);
-
 			// Part 1
-			foreach (var command in input)
-			{
-				char action = command[0];
-				int value = int.Parse(command[1..]);
+			(int x, int y) location = (0, 0);
+			(int x, int y) waypoint = (1, 0);
 
-				switch (action)
-				{
-					case 'N':
-						location.y += value;
-						break;
-					case 'S':
-						location.y -= value;
-						break;
-					case 'E':
-						location.x += value;
-						break;
-					case 'W':
-						location.x -= value;
-						break;
-
-					case 'L':
-						dir = (Direction)((int)dir - (value / 90) & 3);
-						break;
-					case 'R':
-						dir = (Direction)((int)dir + (value / 90) & 3);
-						break;
-
-					case 'F':
-						if (dir == Direction.North)
-						{
-							location.y += value;
-							break;
-						}
-						else if (dir == Direction.South)
-						{
-							location.y -= value;
-							break;
-						}
-						else if (dir == Direction.East)
-						{
-							location.x += value;
-							break;
-						}
-						else
-						{
-							location.x -= value;
-							break;
-						}
-
-					default:
-						break;
-				}
-			}
-
+			ExecuteCommands(1, input, ref location, waypoint);
 			int EW = Math.Abs(location.x);
 			int NS = Math.Abs(location.y);
 			Console.WriteLine($"Part 1: {EW} + {NS} = {EW + NS}");
 
 			// Part 2
-			location = new(0, 0);
-			(int x, int y) waypoint = (10, 1);
+			location = (0, 0);
+			waypoint = (10, 1);
+
+			ExecuteCommands(2, input, ref location, waypoint);
+			EW = Math.Abs(location.x);
+			NS = Math.Abs(location.y);
+			Console.WriteLine($"Part 2: {EW} + {NS} = {EW + NS}");
+		}
+
+		private static void ExecuteCommands(int part, string[] input, ref (int x, int y) location, (int x, int y) waypoint)
+		{
+			ref (int x, int y) target = ref part == 1 ? ref location : ref waypoint;
 
 			foreach (var command in input)
 			{
@@ -89,43 +38,33 @@ namespace Day12
 				int value = int.Parse(command[1..]);
 
 				int temp;
+
 				switch (action)
 				{
 					case 'N':
-						waypoint.y += value;
+						target.y += value;
 						break;
 					case 'S':
-						waypoint.y -= value;
+						target.y -= value;
 						break;
 					case 'E':
-						waypoint.x += value;
+						target.x += value;
 						break;
 					case 'W':
-						waypoint.x -= value;
+						target.x -= value;
 						break;
 
 					case 'L':
-						switch (value)
+						// Flip and "Fallthrough"
+						if (value == 90)
 						{
-							case 90:
-								temp = waypoint.x;
-								waypoint.x = -waypoint.y;
-								waypoint.y = temp;
-								break;
-
-							case 180:
-								waypoint.x = -waypoint.x;
-								waypoint.y = -waypoint.y;
-								break;
-							case 270:
-								temp = waypoint.x;
-								waypoint.x = waypoint.y;
-								waypoint.y = -temp;
-								break;
-							default:
-								break;
+							value = 270;
 						}
-						break;
+						else if (value == 270)
+						{
+							value = 90;
+						}
+						goto case 'R';						
 					case 'R':
 						switch (value)
 						{
@@ -146,6 +85,7 @@ namespace Day12
 							default:
 								break;
 						}
+
 						break;
 
 					case 'F':
@@ -157,10 +97,6 @@ namespace Day12
 						break;
 				}
 			}
-
-			EW = Math.Abs(location.x);
-			NS = Math.Abs(location.y);
-			Console.WriteLine($"Part 2: {EW} + {NS} = {EW + NS}");
 		}
 	}
 }
