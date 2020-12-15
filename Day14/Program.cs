9637850f5a99b1c51cca08e482d319caf541be37
Spec-Chum <specchumster@gmail.com>
2020-12-15 02:18:01 +0000

    Swapped parts around and added message for each part

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -14,6 +14,55 @@ namespace Day14
 			Part2(input);
 		}
 
+		static void Part1(string[] input)
+		{
+			long[] mem = new long[65536];
+			long maskZeros = 0;
+			long maskOnes = 0;
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				var line = input[i].AsSpan();
+				if (line.StartsWith("ma"))
+				{
+					maskZeros = 0;
+					maskOnes = 0;
+
+					var mask = line[7..];
+					for (int j = 0; j < mask.Length; j++)
+					{
+						switch (mask[j])
+						{
+							case '0':
+								maskZeros |= 1L << (mask.Length - 1 - j);
+								break;
+							case '1':
+								maskOnes |= 1L << (mask.Length - 1 - j);
+								break;
+							default:
+								continue;
+						}
+					}
+				}
+				else
+				{
+					int addrEnd = line.IndexOf(']');
+					int address = int.Parse(line[4..addrEnd]);
+					long value = long.Parse(line[(addrEnd + 3)..]);
+
+					mem[address] = (value | maskOnes) & ~maskZeros;
+				}
+			}
+
+			long total = 0;
+			for (int i = 0; i < mem.Length; i++)
+			{
+				total += mem[i];
+			}
+
+			Console.WriteLine($"Part 1: {total}");
+		}
+
 		static void Part2(string[] input)
 		{
 			Dictionary<long, long> mem = new();
@@ -68,56 +117,7 @@ namespace Day14
 				total += num.Value;
 			}
 
-			Console.WriteLine(total);
-		}
-
-		static void Part1(string[] input)
-		{
-			long[] mem = new long[65536];
-			long maskZeros = 0;
-			long maskOnes = 0;
-
-			for (int i = 0; i < input.Length; i++)
-			{
-				var line = input[i].AsSpan();
-				if (line.StartsWith("ma"))
-				{
-					maskZeros = 0;
-					maskOnes = 0;
-
-					var mask = line[7..];
-					for (int j = 0; j < mask.Length; j++)
-					{
-						switch (mask[j])
-						{
-							case '0':
-								maskZeros |= 1L << (mask.Length - 1 - j);
-								break;
-							case '1':
-								maskOnes |= 1L << (mask.Length - 1 - j);
-								break;
-							default:
-								continue;
-						}
-					}
-				}
-				else
-				{
-					int addrEnd = line.IndexOf(']');
-					int address = int.Parse(line[4..addrEnd]);
-					long value = long.Parse(line[(addrEnd + 3)..]);
-
-					mem[address] = (value | maskOnes) & ~maskZeros;
-				}
-			}
-
-			long total = 0;
-			for (int i = 0; i < mem.Length; i++)
-			{
-				total += mem[i];
-			}
-
-			Console.WriteLine(total);
+			Console.WriteLine($"Part 2: {total}");
 		}
 	}
 }
