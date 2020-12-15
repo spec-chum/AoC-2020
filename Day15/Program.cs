099395360a34f1630b10f6f9066e79742d05bd81
Spec-Chum <specchumster@gmail.com>
2020-12-15 16:44:02 +0000

    Optimised a little

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -9,6 +9,15 @@ namespace Day15
 		{
 			List<int> input = new() { 16, 1, 0, 18, 12, 14, 19 };
 
+			int total = RunPart(1, input);
+			Console.WriteLine(total);
+
+			total = RunPart(2, input);
+			Console.WriteLine(total);
+		}
+
+		private static int RunPart(int part, List<int> input)
+		{
 			Dictionary<int, int> indexes = new();
 
 			for (int i = 0; i < input.Count; i++)
@@ -16,31 +25,21 @@ namespace Day15
 				indexes[input[i]] = i;
 			}
 
-			int total = RunPart(1, input, indexes);
-			Console.WriteLine(total);
-
-			total = RunPart(2, input, indexes);
-			Console.WriteLine(total);
-		}
-
-		private static int RunPart(int part, List<int> input, Dictionary<int, int> diffs)
-		{
 			int index = input.Count - 1;
-			int currentNum = 0;
+			int currentNum = input[index];
 
-			while (index < (part == 1 ? 2020 : 30_000_000))
+			while (index + 1 < (part == 1 ? 2020 : 30_000_000))
 			{
-				currentNum = input[index];
-
-				if (!diffs.ContainsKey(currentNum))
+				if (!indexes.ContainsKey(currentNum))
 				{
-					diffs[currentNum] = index;
-					input.Add(0);
+					indexes[currentNum] = index;
+					currentNum = 0;
 				}
 				else
 				{
-					input.Add(index - diffs[currentNum]);
-					diffs[currentNum] = index;
+					int temp = indexes[currentNum];
+					indexes[currentNum] = index;
+					currentNum = index - temp;
 				}
 
 				index++;
