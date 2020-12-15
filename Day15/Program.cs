cb972b737b3075b4e3bc41c3a7a18b582d16a843
Spec-Chum <specchumster@gmail.com>
2020-12-15 13:26:58 +0000

    Added Day 15 - Part 1

---
+++ Day15/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day15
+{
+	class Program
+	{
+		static void Main()
+		{
+			List<int> input = new() { 16, 1, 0, 18, 12, 14, 19 };
+
+			int index = input.Count - 1;
+			int currentNum = 0;
+
+			while (index < 2020)
+			{
+				currentNum = input[index];
+				int diff = input.LastIndexOf(currentNum, index - 1);
+
+				if (diff == -1)
+				{
+					input.Add(0);
+				}
+				else
+				{
+					input.Add(index - diff);
+				}
+
+				index++;
+			}
+
+			Console.WriteLine(currentNum);
+		}
+	}
+}
