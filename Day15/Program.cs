08f37b871ed1b5432e95d2f1fe0461b17776fd71
Spec-Chum <specchumster@gmail.com>
2020-12-15 15:44:18 +0000

    Added an albeit very slow, part 2

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -9,27 +9,44 @@ namespace Day15
 		{
 			List<int> input = new() { 16, 1, 0, 18, 12, 14, 19 };
 
+			Dictionary<int, int> indexes = new();
+
+			for (int i = 0; i < input.Count; i++)
+			{
+				indexes[input[i]] = i;
+			}
+
+			int total = RunPart(1, input, indexes);
+			Console.WriteLine(total);
+
+			total = RunPart(2, input, indexes);
+			Console.WriteLine(total);
+		}
+
+		private static int RunPart(int part, List<int> input, Dictionary<int, int> diffs)
+		{
 			int index = input.Count - 1;
 			int currentNum = 0;
 
-			while (index < 2020)
+			while (index < (part == 1 ? 2020 : 30_000_000))
 			{
 				currentNum = input[index];
-				int diff = input.LastIndexOf(currentNum, index - 1);
 
-				if (diff == -1)
+				if (!diffs.ContainsKey(currentNum))
 				{
+					diffs[currentNum] = index;
 					input.Add(0);
 				}
 				else
 				{
-					input.Add(index - diff);
+					input.Add(index - diffs[currentNum]);
+					diffs[currentNum] = index;
 				}
 
 				index++;
 			}
 
-			Console.WriteLine(currentNum);
+			return currentNum;
 		}
 	}
 }
